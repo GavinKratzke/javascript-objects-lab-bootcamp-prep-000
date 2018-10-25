8d1e56716446a3481f1b4c5ccdc5398541653735
Gavin Kratzke <gjkkratzke@gmail.com>
2018-10-25 22:29:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,6 @@ function deleteFromObjectByKey(object, key) {
 
 function destructivelyDeleteFromObjectByKey( object, key) {
   var obj = {prop: 1}
-  console.log(delete obj.prop);
+  delete obj.prop;
   return obj
 }
